af4728a2ab2b703f9043a462242d49c8fe1cd8cf
Ehsan <ehsansouri23@gmail.com>
2019-01-20 21:06:18 +0330

    resize image

--- cv.py
+++ cv.py
@@ -3,7 +3,7 @@ import cv2 as cv
 print(cv.__version__)
 
 # showing oroginal image
-image = cv.imread('test.jpg', cv.IMREAD_COLOR)
+image = cv.imread('test.jpg')
 cv.imshow('original image', image)
 cv.waitKey(0)
 cv.destroyAllWindows()
@@ -20,7 +20,16 @@ cv.imshow('gray image', grayImage)
 cv.waitKey(0)
 cv.destroyAllWindows()
 
+# blurring gray scale image with guassian blur
 blurImage = cv.GaussianBlur(grayImage, (9, 9), 5)
 cv.imshow('gray blur image', blurImage)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# scaling image
+image = cv.imread('test.jpg')
+height, width = image.shape[:2]
+resizedImage = cv.resize(image, (width / 2, height), interpolation=cv.INTER_CUBIC)
+cv.imshow('resized image', resizedImage)
 cv.waitKey(0)
 cv.destroyAllWindows()
