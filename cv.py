1db6636bc1b94606dcafd1bf06ea00c7c847a015
Ehsan <ehsansouri23@gmail.com>
2019-01-20 20:37:45 +0330

    remove blue color from image

--- cv.py
+++ cv.py
@@ -2,10 +2,21 @@ import cv2 as cv
 
 print(cv.__version__)
 
+# showing oroginal image
 image = cv.imread('test.jpg', cv.IMREAD_COLOR)
 cv.imshow('original image', image)
 cv.waitKey(0)
 cv.destroyAllWindows()
+
+# showing image without blue
+image[:, :, 0] = 0
+cv.imshow('without blue image', image)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# showing gray scale image
 grayImage = cv.imread('test.jpg', cv.IMREAD_GRAYSCALE)
 cv.imshow('gray image', grayImage)
 cv.waitKey(0)
+cv.destroyAllWindows()
+
